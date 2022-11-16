66b58b6a6770ce05080ccf0012771882c82b790b
yahsya24 <yayahya2004@gmail.com>
2022-11-17 00:23:17 +0700

    i

--- pages/bahanpokok.php
+++ pages/bahanpokok.php
@@ -1,9 +1,242 @@
-<h1>BAHAN POKOK</h1>
+<style>
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        max-width: 300px;
+        margin: auto;
+        text-align: center;
+        font-family: arial;
+        display: inline-block;
+        width: 300px;
+        margin: 5px;
+        height: 450px;
+        max-height: 450px;
+        overflow: hidden;
+    }
 
-<h2>1. Gula Kristal Putih</h2>
-<h2>2. Beras</h2>
-<h2>3. Tepung Terigu</h2>
-<h2>4. Minyak Goreng</h2>
-<h2>5. Garam</h2>
-<h2>6. Telur</h2>
-<h2>7. Beras Ketan</h2>
+    .price {
+        color: grey;
+        font-size: 22px;
+    }
+
+    .card button {
+        border: none;
+        outline: 0;
+        padding: 12px;
+        color: white;
+        background-color: #000;
+        text-align: center;
+        cursor: pointer;
+        width: 100%;
+        font-size: 18px;
+    }
+
+    .card button:hover {
+        opacity: 0.7;
+    }
+</style>
+
+
+<h1 style="text-align:center">Product Card</h1>
+
+<h1 style="text-align: center;">Beras</h1>
+
+<div class="kolom">
+
+    <div class="card">
+        <img src="img/beras(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Panganesia</h1>
+        <p class="price">Rp.100.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/brsmerah(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Beras Merah</h1>
+        <p class="price">Rp.101.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/maknyus(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Maknyus</h1>
+        <p class="price">Rp.102.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/pandawanggi(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Panda Wangi</h1>
+        <p class="price">Rp.103.000,00</p>
+        <br>
+        <br>
+    </div>
+
+
+
+</div>
+
+<h1 style="text-align: center;">Tepung</h1>
+<div class="kolom">
+    <div class="card">
+        <img src="img/gelpong(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Segitiga Biru</h1>
+        <p class="price">Rp.15.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/terigu(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Terigu</h1>
+        <p class="price">Rp.6.500,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/gatotkaca(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Gatot kaca</h1>
+        <p class="price">Rp.7.500,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/cakrakembar(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Cakra Kembar</h1>
+        <p class="price">Rp.8.500,00</p>
+        <br>
+        <br>
+    </div>
+</div>
+
+
+<h1 style="text-align: center;">Garam</h1>
+<div class="kolom">
+    <div class="card">
+        <img src="img/uyah(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Kapal</h1>
+        <p class="price">Rp.6.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/lodan(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Lodan</h1>
+        <p class="price">Rp.6.500,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/dolpin(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Dolpin</h1>
+        <p class="price">Rp.8.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/daun(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Daun</h1>
+        <p class="price">Rp.8.500,00</p>
+        <br>
+        <br>
+    </div>
+</div>
+
+
+<h1 style="text-align: center;">Minyak Goreng</h1>
+<div class="kolom">
+    <div class="card">
+        <img src="img/lengo(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Bimoli</h1>
+        <p class="price">Rp.25.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/filma(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Filma</h1>
+        <p class="price">Rp.14.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/sunco(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Sunco</h1>
+        <p class="price">Rp.11.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/fortune(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Fortune</h1>
+        <p class="price">Rp.20.000,00</p>
+        <br>
+        <br>
+    </div>
+</div>
+
+
+<h1 style="text-align: center;">Kecap</h1>
+
+<div class="kolom">
+    <div class="card">
+        <img src="img/kecapasin(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Kecap Asin</h1>
+        <p class="price">Rp.23.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/kecapabc(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Kecap Abc</h1>
+        <p class="price">Rp.14.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/indofood(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Indofood</h1>
+        <p class="price">Rp.15.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/bango(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Bango</h1>
+        <p class="price">Rp.25.000,00</p>
+        <br>
+        <br>
+    </div>
+</div>
+
+
+<h1 style="text-align: center;">Mie Instan</h1>
+
+<div class="kolom">
+    <div class="card">
+        <img src="img/samyang(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Mie Samyang</h1>
+        <p class="price">Rp.10.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/supermie(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Supermie</h1>
+        <p class="price">Rp.2.500,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/indomie(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Indomie</h1>
+        <p class="price">Rp.3.000,00</p>
+        <br>
+        <br>
+    </div>
+    <div class="card">
+        <img src="img/sedap(1).jpg" alt="Denim Jeans" style="width:100%">
+        <h1>Mie Sedap</h1>
+        <p class="price">Rp.2.000,00</p>
+        <br>
+        <br>
+    </div>
+</div>
