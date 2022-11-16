5fd9767a3fd47bef926e8d4cca2d9676a7441c9a
yahsya24 <yayahya2004@gmail.com>
2022-11-16 22:03:47 +0700

    fhy

--- pages/bahanpokok.php
+++ pages/bahanpokok.php
@@ -1,3 +1,9 @@
-<div class="pd">
-    <h1>Bahan Pertama</h1>
-</div>
+<h1>BAHAN POKOK</h1>
+
+<h2>1. Gula Kristal Putih</h2>
+<h2>2. Beras</h2>
+<h2>3. Tepung Terigu</h2>
+<h2>4. Minyak Goreng</h2>
+<h2>5. Garam</h2>
+<h2>6. Telur</h2>
+<h2>7. Beras Ketan</h2>
