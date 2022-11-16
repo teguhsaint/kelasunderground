4203b3e33f2326a6b9c48bd8c5e9fa23f7cd06c7
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2022-11-17 00:35:57 +0700

    s

--- pages/bahanpokok.php
+++ pages/bahanpokok.php
@@ -1,70 +1,32 @@
-<style>
-    .card {
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        max-width: 300px;
-        margin: auto;
-        text-align: center;
-        font-family: arial;
-        display: inline-block;
-        width: 300px;
-        margin: 5px;
-        height: 450px;
-        max-height: 450px;
-        overflow: hidden;
-    }
-
-    .price {
-        color: grey;
-        font-size: 22px;
-    }
-
-    .card button {
-        border: none;
-        outline: 0;
-        padding: 12px;
-        color: white;
-        background-color: #000;
-        text-align: center;
-        cursor: pointer;
-        width: 100%;
-        font-size: 18px;
-    }
-
-    .card button:hover {
-        opacity: 0.7;
-    }
-</style>
-
-
 <h1 style="text-align:center">Product Card</h1>
 
 <h1 style="text-align: center;">Beras</h1>
 
 <div class="kolom">
 
-    <div class="card">
-        <img src="img/beras(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/beras(1).jpg" alt="Denim Jeans">
         <h1>Panganesia</h1>
         <p class="price">Rp.100.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/brsmerah(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/brsmerah(1).jpg" alt="Denim Jeans">
         <h1>Beras Merah</h1>
         <p class="price">Rp.101.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/maknyus(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/maknyus(1).jpg" alt="Denim Jeans">
         <h1>Maknyus</h1>
         <p class="price">Rp.102.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/pandawanggi(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/pandawanggi(1).jpg" alt="Denim Jeans">
         <h1>Panda Wangi</h1>
         <p class="price">Rp.103.000,00</p>
         <br>
@@ -77,29 +39,29 @@
 
 <h1 style="text-align: center;">Tepung</h1>
 <div class="kolom">
-    <div class="card">
-        <img src="img/gelpong(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/gelpong(1).jpg" alt="Denim Jeans">
         <h1>Segitiga Biru</h1>
         <p class="price">Rp.15.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/terigu(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/terigu(1).jpg" alt="Denim Jeans">
         <h1>Terigu</h1>
         <p class="price">Rp.6.500,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/gatotkaca(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/gatotkaca(1).jpg" alt="Denim Jeans">
         <h1>Gatot kaca</h1>
         <p class="price">Rp.7.500,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/cakrakembar(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/cakrakembar(1).jpg" alt="Denim Jeans">
         <h1>Cakra Kembar</h1>
         <p class="price">Rp.8.500,00</p>
         <br>
@@ -110,29 +72,29 @@
 
 <h1 style="text-align: center;">Garam</h1>
 <div class="kolom">
-    <div class="card">
-        <img src="img/uyah(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/uyah(1).jpg" alt="Denim Jeans">
         <h1>Kapal</h1>
         <p class="price">Rp.6.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/lodan(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/lodan(1).jpg" alt="Denim Jeans">
         <h1>Lodan</h1>
         <p class="price">Rp.6.500,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/dolpin(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/dolpin(1).jpg" alt="Denim Jeans">
         <h1>Dolpin</h1>
         <p class="price">Rp.8.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/daun(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/daun(1).jpg" alt="Denim Jeans">
         <h1>Daun</h1>
         <p class="price">Rp.8.500,00</p>
         <br>
@@ -143,29 +105,29 @@
 
 <h1 style="text-align: center;">Minyak Goreng</h1>
 <div class="kolom">
-    <div class="card">
-        <img src="img/lengo(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/lengo(1).jpg" alt="Denim Jeans">
         <h1>Bimoli</h1>
         <p class="price">Rp.25.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/filma(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/filma(1).jpg" alt="Denim Jeans">
         <h1>Filma</h1>
         <p class="price">Rp.14.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/sunco(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/sunco(1).jpg" alt="Denim Jeans">
         <h1>Sunco</h1>
         <p class="price">Rp.11.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/fortune(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/fortune(1).jpg" alt="Denim Jeans">
         <h1>Fortune</h1>
         <p class="price">Rp.20.000,00</p>
         <br>
@@ -177,29 +139,29 @@
 <h1 style="text-align: center;">Kecap</h1>
 
 <div class="kolom">
-    <div class="card">
-        <img src="img/kecapasin(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/kecapasin(1).jpg" alt="Denim Jeans">
         <h1>Kecap Asin</h1>
         <p class="price">Rp.23.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/kecapabc(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/kecapabc(1).jpg" alt="Denim Jeans">
         <h1>Kecap Abc</h1>
         <p class="price">Rp.14.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/indofood(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/indofood(1).jpg" alt="Denim Jeans">
         <h1>Indofood</h1>
         <p class="price">Rp.15.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/bango(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/bango(1).jpg" alt="Denim Jeans">
         <h1>Bango</h1>
         <p class="price">Rp.25.000,00</p>
         <br>
@@ -211,29 +173,29 @@
 <h1 style="text-align: center;">Mie Instan</h1>
 
 <div class="kolom">
-    <div class="card">
-        <img src="img/samyang(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/samyang(1).jpg" alt="Denim Jeans">
         <h1>Mie Samyang</h1>
         <p class="price">Rp.10.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/supermie(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/supermie(1).jpg" alt="Denim Jeans">
         <h1>Supermie</h1>
         <p class="price">Rp.2.500,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/indomie(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/indomie(1).jpg" alt="Denim Jeans">
         <h1>Indomie</h1>
         <p class="price">Rp.3.000,00</p>
         <br>
         <br>
     </div>
-    <div class="card">
-        <img src="img/sedap(1).jpg" alt="Denim Jeans" style="width:100%">
+    <div class="card grid-item ">
+        <img src="img/sedap(1).jpg" alt="Denim Jeans">
         <h1>Mie Sedap</h1>
         <p class="price">Rp.2.000,00</p>
         <br>
