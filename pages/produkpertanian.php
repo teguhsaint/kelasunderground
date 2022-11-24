468b28dd98761d6eda69e940ef7a6adce03632eb
mjazaia <118255055+mjazaia@users.noreply.github.com>
2022-11-24 11:30:51 -0800

    ppppp

--- pages/produkpertanian.php
+++ pages/produkpertanian.php
@@ -1,8 +1,114 @@
 <style>    
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 210px;
+}
+
+div.gallery:hover {
+  border: 2px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 1px;
+  text-align: center;
+}
+
 </style>
-<h1 style="text-align:center">Produk Prtanian</h1>
 
 
-<div class="pd">
-    <h1>Bahan Pertanian</h1>
+<head>
+<h1 style="text-align:center">Products Prtanian</h1>
+<h1 style="text-align:center">BENIH PADI</h1>
+</head>
+
+<body>
+<div class="gallery">
+  <a target="_blank" href="./img/padi01.png">
+    <img src="./img/padi01.png" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Benih Pertiwi - Benih Padi Unggul INPARI 42 AGRITAN GSR</div>
+  <div class="desc">
+    <h3>Mulai dari Rp 70.000,00</h3></div>
 </div>
+
+<div class="gallery">
+  <a target="_blank" href="./img/padi02.png">
+    <img src="./img/padi02.png" alt="Forest" width="600" height="400">
+  </a>
+  <p></p>
+  <div class="desc">Benih Padi Cakrabuana 04 Istimewa</div>
+  <p></p>
+  <div class="desc">
+    <h3>Mulai dari Rp 60.000,00</h3></div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="./img/padi03.png">
+    <img src="./img/padi03.png" alt="Northern Lights" width="600" height="400">
+  </a>
+  <p></p>
+  <div class="desc">Benih Padi Hitam Organik</div>
+  <div class="desc"><h3>Mulai dari Rp 65.000,00</h3></div>
+</div>
+
+<div class="gallery">
+    <p></p>
+  <a target="_blank" href="./img/padi04.png">
+    <img src="./img/padi04.png" alt="Mountains" width="600" height="400">
+  </a>
+  <p></p>
+  <div class="desc">Penih Padi Sertani 14</div>
+  <p></p>
+  <div class="desc"><h3>Mulai dari Rp 55.000,00</h3></div>
+</div>
+
+<div class="gallery">
+    <p></p>
+  <a target="_blank" href="./img/padi05.png">
+    <img src="./img/padi05.png" alt="Northern Lights" width="600" height="400">
+  </a>
+  <p></p>
+  <div class="desc">Beni Padi Basmati Aroma</div>
+  <div class="desc"><h3>Mulai dari Rp 75.000,00</h3></div>
+</div>
+
+<div class="gallery">
+    <p></p>
+  <a target="_blank" href="./img/padi06.png">
+    <img src="./img/padi06.png" alt="Northern Lights" width="600" height="400">
+  </a>
+  <p></p>
+  <div class="desc">Beni Padi SR Genjah</div>
+   <div class="desc"><h3>Mulai dari Rp 60.000,00</h3></div>
+</div>
+</body>
+
+<head>
+    <h1 style="text-align:center"> </h1>
+</head>
+<body>
+    <div class="gallery">
+    <p></p>
+  <a target="_blank" href="./img/padi06.png">
+    <img src="./img/padi06.png" alt="Northern Lights" width="600" height="400">
+  </a>
+  <p></p>
+  <div class="desc">Beni Padi SR Genjah</div>
+   <div class="desc"><h3>Mulai dari Rp 60.000,00</h3></div>
+</div>
+</body>
+
+
+
+
+
+
+
+
